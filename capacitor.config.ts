5feab2d524f2b479a9da019861e462521f7e97d0
Salsalysj <solisif87@gmail.com>
2026-02-26 12:08:40 +0900

    app_v2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,6 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'kr.gcalc.app',
   appName: '껨산기',
-  bundledWebRuntime: false,
   server: {
     url: 'https://www.gcalc.kr',
     cleartext: false,
